f5bb1d95e0fce75a234cbc5054cc4a31082d97d8
Lucas Weakland <lm.weakland@setonhill.edu>
2020-04-28 13:40:35 -0400

    Initial Commit

---
+++ Weakland Final Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weakland Final Project
+//
+//  Created by Lucas Weakland on 4/28/20.
+//  Copyright © 2020 Lucas Weakland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
